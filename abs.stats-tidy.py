bed1f580dea662a807cb67c6d85dd6310a6f9b50
lambert <44629854+nicjlambert@users.noreply.github.com>
2024-06-02 13:59:13 +0000

    updated with read xl and clean sctns

--- abs.stats-tidy.py
+++ abs.stats-tidy.py
@@ -10,8 +10,27 @@ with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_file:
     temp_file_path = temp_file.name
     urllib.request.urlretrieve(url, temp_file_path)
 
-# Read the data from the downloaded ABS spreadsheet
-df = pd.read_excel(temp_file_path)
+# function to read an excel file and take as an argument the sheet number
+def readXL(temp_file_path, sheet_nbr, header):
+    return pd.read_excel(temp_file_path, sheet_name = sheet_nbr, header = header)
 
-# Display the first few rows of the DataFrame
-print(df.head())
+df_headers = readXL(temp_file_path, 0, header = 9)
+df_data1 = readXL(temp_file_path, 1, header = 9)
+
+def clean_data(df):
+    df = df.rename(columns={"Series ID": "Time Period"})
+    df = df.melt(id_vars=["Time Period"], var_name="Series ID", value_name="Observation Value")
+    return df
+
+def clean_headers(df):
+    df = df.dropna(axis=0, how="all")
+    df = df[~df['Data Item Description'].str.contains('© Commonwealth of Australia', na=False)]
+    df = df.dropna(axis=1, how='all')
+  
+    return df
+
+df_headers = clean_headers(df_headers)
+df_data1 = clean_data(df_data1)
+
+print(df_data1.head())
+print(df_headers.tail())
